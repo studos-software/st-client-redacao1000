d89df7db970b4e9ed3133251ec0fd88bb236e02f
Thiago Paes <mrprompt@gmail.com>
2021-01-07 15:30:47 -0300

    Add class subscription

--- tests/Escola/EscolaTest.php
+++ tests/Escola/EscolaTest.php
@@ -4,6 +4,7 @@ namespace Studos\Redacao1000\Tests\Escola;
 use Faker\Factory;
 use GuzzleHttp\Psr7\Response;
 use Studos\Redacao1000\Escola;
+use Studos\Redacao1000\HttpClient;
 use Studos\Redacao1000\Tests\Base;
 
 class EscolaTest extends Base
@@ -43,6 +44,70 @@ class EscolaTest extends Base
         );
 
         $this->assertArrayHasKey('escolaId', $result['data']);
+    }
+
+    /**
+     * @test
+     * @dataProvider subscribeClassDataProvider
+     * @param string $year
+     * @param string $codeSchool
+     * @param string $codeClass
+     * @param string $name
+     */
+    public function subscribeClass(string $year, string $codeSchool, string $codeClass, string $name)
+    {
+        $handleResponse = $this->fixture(__FUNCTION__, 'Responses');
+        $handlerStack = [new Response(200, [], $handleResponse)];
+
+        $this->client = $this->getClient($handlerStack);
+
+        $service = new Escola($this->client);
+        $result = $service->signupClass(
+            $year,
+            $codeSchool,
+            $codeClass,
+            $name
+        );
+
+        $this->assertArrayHasKey('turmaId', $result['data']);
+    }
+
+    /**
+     * @test
+     */
+    public function subscribeClassWithInvalidCodeSchool()
+    {
+        $handleResponse = $this->fixture(__FUNCTION__, 'Responses');
+        $handlerStack = [new Response(200, [], $handleResponse)];
+
+        $this->client = $this->getClient($handlerStack);
+
+        $service = new Escola($this->client);
+        $result = $service->signupClass(
+            $this->faker->year,
+            $this->faker->randomNumber(4),
+            $this->faker->randomNumber(4),
+            $this->faker->name
+        );
+
+        $this->assertArrayHasKey('error', $result);
+    }
+
+    /**
+     * @return \array[][]
+     */
+    public function subscribeClassDataProvider(): array
+    {
+        $faker = Factory::create();
+
+        return [
+            'fullData' => [
+                '1º ano',
+                '1',
+                (string) $faker->randomNumber(4),
+                $faker->name,
+            ],
+        ];
     }
 
     /**
