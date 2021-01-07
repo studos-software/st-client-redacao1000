7700e9c91dd16f607a4e8de6f7f7144c58fc58db
Thiago Paes <mrprompt@gmail.com>
2021-01-07 17:23:23 -0300

    Add student api

---
+++ src/Student.php
@@ -0,0 +1,48 @@
+<?php
+namespace Studos\Redacao1000;
+
+final class Student extends Base
+{
+    /**
+     * Efetua o cadastramento de um aluno associado a uma turma e a um parceiro.
+     * O acesso para o serviço de cadastro de escola deverá ser feito pelo parceiro autenticado na API
+     * de login.
+     *
+     * @param string $codeStudent
+     * @param string $codeSchool
+     * @param string $codeRegistration
+     * @param string $codeSchollClass
+     * @param string $email
+     * @param string $name
+     * @param int|null $taskId
+     * @param bool $generateAccessToken
+     * @return array
+     */
+    public function signup(
+        string $codeStudent,
+        string $codeSchool,
+        string $codeRegistration,
+        string $codeSchollClass,
+        string $email,
+        string $name,
+        int $taskId = null,
+        bool $generateAccessToken = false
+    ): array {
+        $params = [
+            "codigoAlunoParceiro" => $codeStudent,
+            "codigoEscolaParceiro" => $codeSchool,
+            "codigoMatricula" => $codeRegistration,
+            "codigoTurmaParceiro" => $codeSchollClass,
+            "email" => $email,
+            "nome" => $name,
+            "gerarTokenAcesso" => $generateAccessToken
+
+        ];
+
+        if (!empty($taskId)) {
+            $params['tarefaId'] = $taskId;
+        }
+
+        return $this->request('POST', 'aluno', $params);
+    }
+}
---
+++ tests/Student/StudentTest.php
@@ -0,0 +1,82 @@
+<?php
+namespace Studos\Redacao1000\Tests\School;
+
+use Faker\Factory;
+use GuzzleHttp\Psr7\Response;
+use Studos\Redacao1000\Student;
+use Studos\Redacao1000\Tests\Base;
+
+class StudentTest extends Base
+{
+    /**
+     * @test
+     * @dataProvider signupStudentDataProvider
+     * @param string $codeStudent
+     * @param string $codeSchool
+     * @param string $codeRegistration
+     * @param string $codeSchollClass
+     * @param string $email
+     * @param string $name
+     * @param int|null $taskId
+     * @param bool $generateAccessToken
+     */
+    public function signupStudent(
+        string $codeStudent,
+        string $codeSchool,
+        string $codeRegistration,
+        string $codeSchollClass,
+        string $email,
+        string $name,
+        int $taskId = null,
+        bool $generateAccessToken = false
+    )
+    {
+        $handleResponse = $this->fixture(__FUNCTION__, 'Responses');
+        $handlerStack = [new Response(200, [], $handleResponse)];
+
+        $this->client = $this->getClient($handlerStack);
+
+        $service = new Student($this->client);
+        $result = $service->signup(
+            $codeStudent,
+            $codeSchool,
+            $codeRegistration,
+            $codeSchollClass,
+            $email,
+            $name,
+            $taskId,
+            $generateAccessToken
+        );
+
+        $this->assertArrayHasKey('alunoId', $result['data']);
+    }
+
+    /**
+     * @return \array[][]
+     */
+    public function signupStudentDataProvider(): array
+    {
+        $faker = Factory::create();
+        
+        return [
+            'fullData' => [
+                (string) $faker->randomNumber(4),
+                (string) $faker->randomNumber(4),
+                (string) $faker->randomNumber(4),
+                (string) $faker->randomNumber(4),
+                $faker->email,
+                $faker->name,
+                $faker->randomNumber(4),
+                true
+            ],
+            'withoutOptionalParameters' => [
+                (string) $faker->randomNumber(4),
+                (string) $faker->randomNumber(4),
+                (string) $faker->randomNumber(4),
+                (string) $faker->randomNumber(4),
+                $faker->email,
+                $faker->name
+            ],
+        ];
+    }
+}
